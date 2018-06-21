e2e1b8b0669f28776283c54e1b94fe84ccd7aa5e
subhita <subhitachoudhary11@gmail.com>
2018-06-21 12:16:38 +0530

    validate component with prop types

--- src/Components/Todos/TodoList.js
+++ src/Components/Todos/TodoList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { TodoItem } from './TodoItem';
+import PropTypes from 'prop-types';
 
 export const TodoList = (props) => {
     return (
@@ -11,4 +12,8 @@ export const TodoList = (props) => {
             </ul>
       </div>  
     )
+}
+
+TodoList.propTypes = {
+    todos: PropTypes.array.isRequired
 }
